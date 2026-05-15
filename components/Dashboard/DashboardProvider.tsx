87248c68152f60539a2b561b6ae4674fd482a5d2
lvd536 <nbikiti@gmail.com>
2026-05-16 03:26:50 +0400

    - feat: dashboard provider changes - delete header

--- components/Dashboard/DashboardProvider.tsx
+++ components/Dashboard/DashboardProvider.tsx
@@ -1,15 +1,8 @@
 "use client";
 
 import { AppSidebar } from "@/components/Dashboard/AppSidebar";
-import {
-    SidebarInset,
-    SidebarProvider,
-    SidebarTrigger,
-} from "@/components/ui/sidebar";
+import { SidebarInset, SidebarProvider } from "@/components/ui/sidebar";
 import { useUserStore } from "@/stores/user.store";
-import { Link } from "next-view-transitions";
-import { Avatar, AvatarFallback, AvatarImage } from "../ui/avatar";
-import Notifications from "../Notifications";
 
 export default function DashboardProvider({
     children,
@@ -21,30 +14,7 @@ export default function DashboardProvider({
     return (
         <SidebarProvider>
             <AppSidebar />
-            <SidebarInset>
-                <header className="flex h-16 shrink-0 items-center justify-between gap-2 transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-12">
-                    <SidebarTrigger className="-ml-1 px-4" />
-                    <h1 className="flex items-center gap-3 font-semibold text-2xl leading-[140%] text-foreground">
-                        WMS Dashboard
-                    </h1>
-                    <div className="flex gap-2 items-center justify-between">
-                        <Notifications />
-                        <Link href="/profile">
-                            <Avatar>
-                                <AvatarImage
-                                    src={user?.avatar_path ?? undefined}
-                                />
-                                <AvatarFallback>
-                                    {user!.name!.slice(0, 2).toUpperCase()}
-                                </AvatarFallback>
-                            </Avatar>
-                        </Link>
-                    </div>
-                </header>
-                <div className="flex flex-1 flex-col gap-4 p-4 pt-0 min-h-screen">
-                    {children}
-                </div>
-            </SidebarInset>
+            <SidebarInset>{children}</SidebarInset>
         </SidebarProvider>
     );
 }
