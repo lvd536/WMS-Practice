fffeb62a2893cd527a273f0093c161630fa50706
lvd536 <nbikiti@gmail.com>
2026-05-21 13:42:57 +0400

    - feat: refactor log.actions.ts

--- actions/logs.actions.ts
+++ actions/logs.actions.ts
@@ -4,67 +4,21 @@ import { createClient } from "@/lib/supabase/server";
 import {
     IInventoryMovement,
     IProductInventoryMovement,
-    IWarehouseInventoryMovement,
+    IDetailedInventoryMovement,
 } from "@/types/warehouse.types";
-import { revalidatePath } from "next/cache";
 
-export async function getRackInventoryMovements(
-    rackId: number,
-    selectFromRack: boolean = true,
-    selectToRack: boolean = true,
-) {
+export async function getOrganizationInventoryMovements(orgId: number) {
     try {
         const supabase = await createClient();
 
-        const conditions = [];
-
-        if (selectFromRack || !selectToRack) {
-            conditions.push(`from_rack_id.eq.${rackId}`);
-        }
-
-        if (selectToRack) {
-            conditions.push(`to_rack_id.eq.${rackId}`);
-        }
-
-        let query = supabase.from("inventory_movements").select();
-
-        if (conditions.length > 0) query = query.or(conditions.join(","));
-
-        const { data, error } = await query;
-
-        if (error) throw new Error(error.message);
-
-        revalidatePath(
-            "/organizations/[organizationId]/warehouses/[warehouseId]/racks/[rackId]",
-            "page",
+        const { data: movements, error } = await supabase.rpc(
+            "get_org_inventory_movements",
+            { p_org_id: orgId },
         );
 
-        return data as IInventoryMovement[];
-    } catch (err) {
-        console.error("getRackInventoryMovements error:", err);
-        return {
-            status: "error",
-            error: {
-                message:
-                    err instanceof Error
-                        ? err.message
-                        : "Unexpected error occurred",
-            },
-        };
-    }
-}
-
-export async function getOrganizationInventoryMovements() {
-    try {
-        const supabase = await createClient();
-
-        const { data, error } = await supabase
-            .from("inventory_movements")
-            .select();
-
         if (error) throw new Error(error.message);
 
-        return data as IInventoryMovement[];
+        return movements as IDetailedInventoryMovement[];
     } catch (err) {
         console.error("getOrganizationInventoryMovements error:", err);
         return {
@@ -90,9 +44,35 @@ export async function getWarehouseInventoryMovements(warehouseId: number) {
 
         if (error) throw new Error(error.message);
 
-        return movements as IWarehouseInventoryMovement[];
+        return movements as IDetailedInventoryMovement[];
     } catch (err) {
         console.error("getWarehouseInventoryMovements error:", err);
+        return {
+            status: "error",
+            error: {
+                message:
+                    err instanceof Error
+                        ? err.message
+                        : "Unexpected error occurred",
+            },
+        };
+    }
+}
+
+export async function getRackMovements(rackId: number) {
+    try {
+        const supabase = await createClient();
+
+        const { data, error } = await supabase
+            .from("inventory_movements")
+            .select()
+            .or(`from_rack_id.eq.${rackId}, to_rack_id.eq.${rackId}`);
+
+        if (error) throw new Error(error.message);
+
+        return data as IInventoryMovement[];
+    } catch (err) {
+        console.error("getProductMovements error:", err);
         return {
             status: "error",
             error: {
